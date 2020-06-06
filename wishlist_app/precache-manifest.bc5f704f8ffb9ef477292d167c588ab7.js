3c2738eabc2f9f6e2080dcaa0477780726df6c9d
Robin Nicole <robinnicole@iphonedumbreras.home>
2020-06-06 09:48:41 +0100

    update-react-app

---
+++ wishlist_app/precache-manifest.bc5f704f8ffb9ef477292d167c588ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c352f53d585a999829b13bad1f1f54ee",
+    "url": "/wishlist_app/index.html"
+  },
+  {
+    "revision": "c336833d2ddb75744201",
+    "url": "/wishlist_app/static/css/main.af6b511c.chunk.css"
+  },
+  {
+    "revision": "e781816f01623b71f2c6",
+    "url": "/wishlist_app/static/js/2.21bce9bc.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/wishlist_app/static/js/2.21bce9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c336833d2ddb75744201",
+    "url": "/wishlist_app/static/js/main.62065a71.chunk.js"
+  },
+  {
+    "revision": "7385352dc4bce13c4a07",
+    "url": "/wishlist_app/static/js/runtime-main.cc2c3e8b.js"
+  }
+]);
